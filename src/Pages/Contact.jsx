d7092c3bee4ab8ed47f2d57ee893212341d7dc23
M0NDI <armond_93@live.co.uk>
2023-10-11 12:06:47 +0100

    finished contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -12,18 +12,32 @@ const Contact = () => {
       className="text-2xl h-screen h-screen w-screen flex items-center justify-center"
     >
       <div className="contact-items flex flex-col justify-center items-center w-8/12">
+
         <div className="contact-email contact-item flex justify-evenly">
-          <img src={emailLogo} className="h-16" />
-          <p className="flex items-center text-zinc-200 text-4xl">armond.kurti@hotmail.com</p>
+          <div className="flex justify-between w-full">
+            <img src={emailLogo} className="w-16 ml-6" />
+            <p className="flex items-center text-zinc-200 text-4xl">
+              <a href="mailto:armond.kurti@hotmail.com">armond.kurti@hotmail.com</a>
+            </p>
+          </div>
         </div>
+
         <div className="contact-github contact-item flex justify-evenly m-4">
-          <img src={githubLogo} className="h-16" />
-          <p className="flex items-center text-zinc-200 text-4xl"><Link to="https://github.com/M0NDI">https://github.com/M0NDI</Link></p>
+          <div className="flex justify-between w-full">
+            <img src={githubLogo} className="h-16" />
+            <p className="flex items-center text-zinc-200 text-4xl">
+              <Link mailto="https://github.com/M0NDI">https://github.com/M0NDI</Link>
+            </p>
+          </div>
         </div>
-        <div className="contact-github contact-item flex justify-evenly ">
-          <img src={phoneLogo} className="h-16" />
-          <p className="flex items-center text-zinc-200 text-4xl">+44 7916712239</p>
+
+        <div className="contact-phone-number contact-item flex justify-evenly ">
+          <div className="flex justify-between w-full">
+            <img src={phoneLogo} className="h-16 ml-6" />
+            <p className="flex items-center text-zinc-200 text-4xl">+44 7916712239</p>
+          </div>
         </div>
+
       </div>
     </div>
   );
